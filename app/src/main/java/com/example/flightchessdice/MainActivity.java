5a4b71cab2daf81dfd657916a3b14e5bb81b3c61
gaylong9 <2236657293@qq.com>
2020-08-17 00:36:07 +0800

    code done, wait debug

--- app/src/main/java/com/example/flightchessdice/MainActivity.java
+++ app/src/main/java/com/example/flightchessdice/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.flightchessdice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivityTAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.roll_res_textview);
+        textView.setOnClickListener(new View.OnClickListener() {
+            Random random = new Random();
+            int r, g, b, bias = 50, color_bound = 156;
+
+            @Override
+            public void onClick(View v) {
+                r = random.nextInt(color_bound) + bias;
+                g = random.nextInt(color_bound) + bias;
+                b = random.nextInt(color_bound) + bias;
+                textView.setText("" + (random.nextInt(6) + 1));
+                textView.setTextColor(Color.rgb(r, g, b));
+                Log.d(TAG, "onClick: rgb: " + r + "," + g + "," +b);
+            }
+        });
+
+
+
     }
 }
